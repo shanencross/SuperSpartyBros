9486030b6db92a7e970a7c679447b16dd1f9c6ec
shanencross <shanencross@yahoo.com>
2017-06-18 19:02:56 -0700

    some modifications

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -9,22 +9,20 @@ public class Coin : MonoBehaviour {
 
 	// if the player touches the coin, it has not already been taken, and the player can move (not dead or victory)
 	// then take the coin
-	void OnTriggerEnter2D (Collider2D collider)
-	{
+	void OnTriggerEnter2D (Collider2D collider) {
 		if (collider.gameObject.layer == LayerMask.NameToLayer("Player") && (!taken)) {
-			CharacterController2D characterController = collider.gameObject.GetComponentInParent<CharacterController2D>();
-			if (characterController.playerCanMove) {
+			CharacterController2D player = collider.gameObject.GetComponentInParent<CharacterController2D>();
+			if (player.playerCanMove) {
 				// mark as taken so doesn't get taken multiple times
 				taken=true;
 
 				// if explosion prefab is provide, then instantiate it
-				if (explosion)
-				{
+				if (explosion) {
 					Instantiate(explosion,transform.position,transform.rotation);
 				}
 
 				// do the player collect coin thing
-				characterController.CollectCoin(coinValue);
+				player.CollectCoin(coinValue);
 
 				// destroy the coin
 				DestroyObject(this.gameObject);
